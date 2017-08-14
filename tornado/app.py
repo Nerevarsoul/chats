4de471d6d9345949059f0e0032c816f2fc57e128
Neri <keshaeverywhere@gmail.com>
2017-08-14 23:34:03 +0300

    Add main funcion

--- tornado/app.py
+++ tornado/app.py
@@ -1,16 +1,44 @@
+import os
+
 import tornado.ioloop
 import tornado.web
+from tornado.options import define, options, parse_command_line
+
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=False, help="run in debug mode")
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
 
-def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+
+class MessageNewHandler(tornado.web.RequestHandler):
+	pass
+
+
+class MessageUpdatesHandler(tornado.web.RequestHandler):
+	pass
+
+
+def main():
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+            (r"/a/message/new", MessageNewHandler),
+            (r"/a/message/updates", MessageUpdatesHandler),
+            ],
+        cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+        debug=options.debug,
+        )
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+	main()
